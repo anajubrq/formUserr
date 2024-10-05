cc3b168d094d93c13cc29ca06579cf5f25ed7a6b
Fernando Chagas <fnd.chagas@gmail.com>
2024-10-05 15:45:05 -0300

    feat: crud

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,23 @@
-import { useState } from "react"
-import FormUser from "@/components/formUser/formUser"
+"use client";
+
 import { Button } from "@/components/ui/button";
 
-export default function Header() {
-    const [openModal, setOpenModal] = useState(false);
+interface HeaderProps {
+  setOpenModal: () => void;
+}
 
-    return (
-        
-     <section className=" bg-slate-400" >
+export default function Header({ setOpenModal }: HeaderProps) {
+  return (
+    <section className="bg-slate-400">
       <div className="flex justify-between items-center w-full">
-        <h1 className="text-[22px] text-[weight] ml-4 p-2">Users</h1>
-        <Button className="text-[18px] text-[weight] m-5 p-3 bg-blue-700 text-white mr-5 pr-5" onClick={() => setOpenModal(true)}>
+        <h1 className="text-[22px] font-bold ml-4 p-2">Users</h1>
+        <Button 
+          className="text-[18px] font-bold m-5 p-3 bg-blue-700 text-white mr-5" 
+          onClick={setOpenModal}
+        >
           +Add User
         </Button>
       </div>
-      <FormUser isOpen={openModal} 
-      setModalOpen={setOpenModal} 
-    
-      
-       />
     </section>
-       
-    )
+  )
 }
